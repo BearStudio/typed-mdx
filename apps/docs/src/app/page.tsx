1b20ad99602d35cc65e36f5ca67e76742857f244
Yoann Fleury <yoann.fleury@yahoo.com>
2024-01-14 17:17:40 +0000

    feat(doc): cleanup and install tailwindcss

---
+++ apps/docs/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Page(): JSX.Element {
+  return (
+    <main>
+      <h1>typed-mdx documentation</h1>
+
+      <section>
+        <h2>Installation</h2>
+      </section>
+    </main>
+  );
+}
